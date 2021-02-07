e0993e235f7e1dc3d97c444e9601998d61396e51
patrick <blockchainbuddha@gmail.com>
2021-02-07 15:55:24 -0500

    start to setup auth, tailwind

--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,7 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import EthereumAuthClient from '@oneclickdapp/ethereum-auth'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { FetchConfigProvider, useFetchConfig } from '@redwoodjs/web'
 import ReactDOM from 'react-dom'
 
 import { FatalErrorBoundary } from '@redwoodjs/web'
@@ -8,11 +12,51 @@ import Routes from 'src/Routes'
 
 import './index.css'
 
+const ApolloInjector = ({ children }) => {
+  const { uri, headers } = useFetchConfig()
+  let ethereum
+  try {
+    const graphQLClient = new ApolloClient({
+      cache: new InMemoryCache(),
+      uri,
+      headers,
+    })
+    // Default option using Apollo Client
+    const makeRequest = (mutation, variables) =>
+      graphQLClient.mutate({
+        mutation,
+        variables,
+      })
+
+    // Alternative option using graphql-hooks
+    // You'll also need to modify graphQLClient
+    // const makeRequest = (query, variables) =>
+    //   graphQLClient.request({
+    //     query,
+    //     variables,
+    //   })
+
+    ethereum = new EthereumAuthClient({
+      makeRequest,
+      debug: process.NODE_ENV === 'development',
+    })
+  } catch (e) {
+    console.log(e)
+  }
+  return React.cloneElement(children, { client: ethereum })
+}
+
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodApolloProvider>
-      <Routes />
-    </RedwoodApolloProvider>
+    <FetchConfigProvider>
+    <ApolloInjector>
+    <AuthProvider client={ethereum} type="ethereum">
+      <RedwoodApolloProvider>
+        <Routes />
+      </RedwoodApolloProvider>
+    </AuthProvider>
+    </ApolloInjector>
+    </FetchConfigProvider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
 )
